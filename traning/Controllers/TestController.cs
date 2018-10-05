9ca6d0bd31d014a63c0ed5614d5a19a8ee6d5c73
Niels Laursen <Nielsblaursen@gmail.com>
2018-10-05 09:52:06 +0200

    updated View and controller to ViewBag

--- traning/Controllers/TestController.cs
+++ traning/Controllers/TestController.cs
@@ -10,6 +10,17 @@ namespace traning.Controllers
     {
         public ActionResult kage()
         {
+
+            ViewBag.Title = "Super Mario 64";
+            ViewBag.Description = "Super Mario 64 is a 1996 platform video game developed and published by Nintendo for the Nintendo 64.";
+            ViewBag.Characters = new string[]
+            {
+                "Mario",
+                "Princess Peach",
+                "Bowser",
+                "Toad",
+                "Yoshi"
+            };
             return View();
         }
     }
