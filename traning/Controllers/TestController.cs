b926ee8ad371831c18a855481ae4cbee43e1bc8e
Niels Laursen <Nielsblaursen@gmail.com>
2018-10-05 07:59:52 +0200

    first controller

---
+++ traning/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace traning.Controllers
+{
+    public class TestController : Controller
+    {
+        public string kage()
+        {
+            return "hej med dig";
+        }
+    }
+}
